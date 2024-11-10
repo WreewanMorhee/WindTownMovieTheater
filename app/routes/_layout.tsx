5f1c51afd9c96631e18ab10a6bbc2f5d99b42c1a
KyleHuang <haokhhh@gmail.com>
2024-11-10 20:05:59 +0800

    fix: css adjust

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -50,7 +50,7 @@ export default function Index() {
       <GetGoogleLoginRelatedData {...data} />
 
       {list_skeleton && (
-        <div className="z-[3] bg-[--deep-blue]  left-[0px] right-[0px] fixed w-full">
+        <div className="z-[4] bg-[--deep-blue]  left-[0px] right-[0px] fixed w-full">
           <div className=" w-full h-[calc(100svh-var(--header-height))] container mx-auto fone:overflow-scroll p-[--page-margin] pt-[--to-top]">
             <div className="mx-auto items-start content-start  bottom-[0px] left-[0px] grid grid-cols-1 fone:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-[--general-gap] ">
               <BasicSkeleton className="video-card relative shadow-md rounded-[--rounded] overflow-hidden pb-[150%]" />
